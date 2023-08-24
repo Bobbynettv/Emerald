df495ec871bc8af27fe71861aed14021e28220c2
Bobbynettv <134861162+Bobbynettv@users.noreply.github.com>
2023-08-24 11:18:02 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,340 @@
+const display = document.querySelector(".display");
+const input = document.querySelector("#upload");
+let img = document.querySelector("img");
+
+input.addEventListener("change", () => {
+  let reader = new FileReader();
+  reader.readAsDataURL(input.files[0]);
+  reader.addEventListener("load", () => {
+    display.innerHTML = `<img src=${reader.result} alt=''/>`;
+  });
+});
+
+
+function getResult() {
+  var reg = document.getElementById("regNumber").value;
+
+
+if (reg == "45871en"){
+return true;   
+     }
+     
+if (reg == "60928xw"){
+  return true;
+}
+
+if (reg == "75314bs"){
+return true;   
+     }
+if (reg == "23196df"){
+  return true;
+}
+
+if (reg == "86205kt"){
+return true;   
+     }
+if (reg == "14703yr"){
+  return true;
+}
+if (reg == "57928ml"){
+return true;   
+     }
+if (reg == "38216zp"){
+  return true;
+}
+if (reg == "96514uj"){
+return true;   
+     }
+if (reg == "72680hg"){
+  return true;
+}
+if (reg == "31087cv"){
+return true;   
+     }
+if (reg == "69451qa"){
+  return true;
+}
+if (reg == "82715zx"){
+return true;   
+     }
+if (reg == "14357pl"){
+  return true;
+}
+if (reg == "59623wt"){
+return true;   
+     }
+if (reg == "20846mj"){
+  return true;
+}
+if (reg == "48162rb"){
+return true;   
+     }
+if (reg == "75390fd"){
+  return true;
+}
+if (reg == "92618kg"){
+return true;   
+     }
+     
+if (reg == "37541sn"){
+  return true;
+}
+
+if (reg == "61837dv"){
+return true;   
+     }
+if (reg == "95243he"){
+  return true;
+}
+
+if (reg == "13659al"){
+return true;   
+     }
+if (reg == "82470uy"){
+  return true;
+}
+if (reg == "50742xi"){
+return true;   
+     }
+if (reg == "36189cm"){
+  return true;
+}
+if (reg == "71925rk"){
+return true;   
+     }
+if (reg == "28416oj"){
+  return true;
+}
+if (reg == "63094ve"){
+return true;   
+     }
+if (reg == "46521tn"){
+  return true;
+}
+if (reg == "90712qw"){
+return true;   
+     }
+if (reg == "14265fg"){
+  return true;
+}
+if (reg == "58632hz"){
+return true;   
+     }
+if (reg == "27368ep"){
+  return true;
+}
+if (reg == "69427yd"){
+return true;   
+     }
+if (reg == "83154bu"){
+  return true;
+  }
+if (reg == "51987in"){
+return true;   
+     }
+if (reg == "24683mj"){
+  return true;
+}
+if (reg == "76891sa"){
+return true;   
+     }
+     
+if (reg == "32045vl"){
+  return true;
+}
+
+if (reg == "15437rk"){
+return true;   
+     }
+if (reg == "69782wo"){
+  return true;
+}
+
+if (reg == "83104pi"){
+return true;   
+     }
+if (reg == "47628hd"){
+  return true;
+}
+if (reg == "93512xn"){
+return true;   
+     }
+if (reg == "26870ef"){
+  return true;
+}
+if (reg == "57013ml"){
+return true;   
+     }
+if (reg == "40379zr"){
+  return true;
+}
+if (reg == "81956tu"){
+return true;   
+     }
+if (reg == "35127ab"){
+  return true;
+}
+if (reg == "69418xp"){
+return true;   
+     }
+if (reg == "52731vh"){
+  return true;
+}
+if (reg == "18490ld"){
+return true;   
+     }
+if (reg == "82034cn"){
+  return true;
+}
+if (reg == "15689jg"){
+return true;   
+     }
+if (reg == "64827wr"){
+  return true;
+  }
+if (reg == "93175to"){
+return true;   
+     }
+if (reg == "74201qe"){
+  return true;
+}
+if (reg == "28956ki"){
+return true;   
+     }
+if (reg == "53049fh"){
+  return true;
+}
+if (reg == "37681zm"){
+return true;   
+     }
+if (reg == "96124ua"){
+  return true;
+}
+if (reg == "10538eo"){
+return true;   
+     }
+if (reg == "72419db"){
+  return true;
+}
+if (reg == "82375vn"){
+return true;   
+     }
+if (reg == "64713xc"){
+  return true;
+}
+if (reg == "49026mj"){
+return true;   
+     }
+if (reg == "25671rl"){
+  return true;
+}
+if (reg == "68397tf"){
+return true;   
+     }
+if (reg == "89125ps"){
+  return true;
+}
+if (reg == "14386wi"){
+return true;   
+     }
+     
+if (reg == "57214yk"){
+  return true;
+}
+
+if (reg == "30928bc"){
+return true;   
+     }
+if (reg == "78413na"){
+  return true;
+}
+
+if (reg == "61528gf"){
+return true;   
+     }
+if (reg == "42750uh"){
+  return true;
+}
+if (reg == "89264ie"){
+return true;   
+     }
+if (reg == "13659al"){
+  return true;
+}
+if (reg == "82470uy"){
+return true;   
+     }
+if (reg == "50742xi"){
+  return true;
+}
+if (reg == "36189cm"){
+return true;   
+     }
+if (reg == "71925rk"){
+  return true;
+}
+if (reg == "28416oj"){
+return true;   
+     }
+if (reg == "63094ve"){
+  return true;
+}
+if (reg == "46521tn"){
+return true;   
+     }
+if (reg == "90712qw"){
+  return true;
+}
+if (reg == "14265fg"){
+return true;   
+     }
+if (reg == "58632hz"){
+  return true;
+  }
+if (reg == "27368ep"){
+return true;   
+     }
+if (reg == "69427yf"){
+  return true;
+}
+if (reg == "83154bu"){
+return true;   
+     }
+if (reg == "51987in"){
+  return true;
+}
+if (reg == "24683mj"){
+return true;   
+     }
+if (reg == "76891sa"){
+  return true;
+}
+if (reg == "32045vl"){
+return true;   
+     }
+if (reg == "15437rk"){
+  return true;
+}
+if (reg == "69782wo"){
+return true;   
+     }
+if (reg == "83104pi"){
+  return true;
+}
+if (reg == "47628hd"){
+return true;   
+     }
+if (reg == "93512xn"){
+  return true;
+}
+if (reg == "26870ef"){
+return true;   
+     }
+if (reg == "57013ml"){
+  return true;
+}
+else{
+  alert("Input only your approved Emerald Innovative Institute in the input box");
+return false;
+}
+ }
